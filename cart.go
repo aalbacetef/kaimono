19a8bf394045337159f1f37c4d553d19f96e8377
Arturo Albacete <aalbacetef@gmail.com>
2024-10-21 17:40:10 +0200

    remove currency enum (leave it as a string); simplify discount type

--- cart.go
+++ cart.go
@@ -7,22 +7,21 @@ type Cart struct {
 }
 
 type CartItem struct {
-	ProductID string     `json:"product-id"`
+	ID        string     `json:"id"`
 	Quantity  int        `json:"quantity"`
 	Discounts []Discount `json:"discounts"`
 	Price     Price      `json:"price"`
 }
 
 type Price struct {
-	Currency Currency `json:"currency"`
-	Value    float64  `json:"value"`
+	Currency string  `json:"currency"`
+	Value    float64 `json:"value"`
 }
 
 type Discount struct {
-	ID            string       `json:"id"`
-	Type          DiscountType `json:"type"`
-	PercentageOff float64      `json:"percentage-off"`
-	AmountOff     float64      `json:"amount-off"`
+	ID    string       `json:"id"`
+	Type  DiscountType `json:"type"`
+	Value float64      `json:"value"`
 }
 
 type DiscountType string
@@ -30,13 +29,4 @@ type DiscountType string
 const (
 	PercentageDiscount  DiscountType = "percentage"
 	FixedAmountDiscount DiscountType = "fixed-amount"
-)
-
-// ISO 4172 three letter code (can't remember if that is the currency ISO).
-type Currency string
-
-const (
-	USD Currency = "usd"
-	EUR Currency = "euro"
-	BTC Currency = "bitcoin"
 )
